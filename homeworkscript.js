ea9c625817887896315ae7875fd005dd1ba04b16
SydneyARCHT <sstiward@hotmail.com>
2024-07-22 14:16:38 -0700

    First Commit/JS Basics

---
+++ homeworkscript.js
@@ -0,0 +1,46 @@
+
+// 1. Understanding Data Types in JavaScript
+
+// Student Name = John
+let studentName = "John"; 
+
+// Age
+let age = 18; 
+
+// Grade 
+let grade = 12; 
+
+// Attendance Status
+let attendance = 100;
+
+
+console.log(`Student Name: ${studentName}`);
+console.log(`- Age: ${age}`);
+console.log(`- Grade: ${grade}`);
+console.log(`- Attendance: ${attendance}%`);
+
+
+// 2. Exploring JavaScript Operators
+let num1 = 17
+let num2 = 42
+
+// -------- Task 3 ---------
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num2 - num1);
+console.log("Product:", num1 * num2);
+console.log("Quotient:", num2 / num1);
+
+// -------- Task 4 ---------
+console.log("Updated num1:", num1 = 37);
+console.log("Updated num2:", num2 = 29);
+
+// -------- Task 5 ---------
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 greater than num2?", num1 > num2);
+console.log("Is num1 not equal to num2?", num1 != num2);
+
+// -------- Task 6 ---------
+let isPositive = (num1, num2) > 0;
+let isEven = num1 % 2 === 0 || num2 % 2 === 0;;
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
